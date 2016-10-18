8f8666c385005e8130969c12361320faca494d83
avanalfen <austinvanalfen@Van-Alfen-Mac.local>
2016-10-18 16:14:16 -0600

    Work In Progress
    I am able to run the functions individually and send each object to the cloud, I can’t get them back yet, I’m rebuilding my transitions from and to views with the new cloudkit model.

---
+++ VirtualCrowd/Note.swift
@@ -0,0 +1,9 @@
+//
+//  Note.swift
+//  VirtualCrowd
+//
+//  Created by Austin Van Alfen on 10/18/16.
+//  Copyright © 2016 VanRoyal. All rights reserved.
+//
+
+import Foundation
---
+++ VirtualCrowd/NoteController.swift
@@ -0,0 +1,9 @@
+//
+//  NoteController.swift
+//  VirtualCrowd
+//
+//  Created by Austin Van Alfen on 10/18/16.
+//  Copyright © 2016 VanRoyal. All rights reserved.
+//
+
+import Foundation
